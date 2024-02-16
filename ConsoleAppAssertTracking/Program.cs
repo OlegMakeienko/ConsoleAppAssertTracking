3d5c916b5498bfb78a705154e292c77d002f81c8
olegmakeienko <molva83@gmail.com>
2024-02-16 14:36:04 +0100

    modify output

--- ConsoleAppAssertTracking/Program.cs
+++ ConsoleAppAssertTracking/Program.cs
@@ -10,18 +10,23 @@ class Program
     {
         List<Asset> assets = new List<Asset>
         {
-            new Laptop("MacBook", "Pro 2020", new DateTime(2024, 1, 1), 1200, "Office1"),
-            new Laptop("Asus", "Spectrum", new DateTime(2022, 1, 1), 1000, "Office2"),
-            new Laptop("Lenovo", "Hero", new DateTime(2021, 7, 1), 900, "Office3"),
-            new MobilePhone("Iphone", "12", new DateTime(2024, 1, 1), 800, "Office1"),
-            new MobilePhone("Samsung", "Galaxy", new DateTime(2023, 10, 1), 900, "Office2"),
-            new MobilePhone("Nokia", "3300", new DateTime(2020, 1, 1), 700, "Office3"),
+            new Laptop("MacBook", "Pro 2020", new DateTime(2024, 1, 1), 1200, "USA"),
+            new Laptop("Asus", "Spectrum", new DateTime(2022, 1, 1), 1000, "Sweden"),
+            new Laptop("Lenovo", "Hero", new DateTime(2021, 7, 1), 900, "Germany"),
+            new MobilePhone("Iphone", "12", new DateTime(2024, 1, 1), 800, "USA"),
+            new MobilePhone("Samsung", "Galaxy", new DateTime(2023, 10, 1), 900, "Germany"),
+            new MobilePhone("Nokia", "3300", new DateTime(2020, 1, 1), 700, "Sweden"),
         };
+        
+        Console.WriteLine("{0,-10} {1,-10} {2,-10} {3,-10} {4,-10} {5,-10} {6,-10} {7,-10}", 
+            "Type", "Brand", "Model", "Office", "Purchase Date", "Price in USD", "Currency", "Local Price Today (SEK)");
         
         foreach (var asset in assets.OrderBy(a => a.GetType().Name).ThenBy(a => a.PurchaseDate))
         {
             Console.ForegroundColor = asset.GetEndOfLifeStatus();
-            Console.WriteLine($"{asset.GetType().Name} {asset.ModelName} {asset.PurchaseDate}");
+            Console.WriteLine("{0,-10} {1,-10} {2,-10} {3,-10} {4,-10} {5,-10} {6,-10} {7,-10}", 
+                asset.GetType().Name, asset.Brand, asset.ModelName, asset.Office, asset.PurchaseDate.ToString("d"),
+                asset.Price, asset.GetCurrencyBasedOnOffice(), asset.LocalPriceToday);
         }
         Console.ResetColor();
     }
